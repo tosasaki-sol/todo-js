fde62fc67e15a4c39ab2005b073a868a806ffb29
tosasaki-sol <97575304+tosasaki-sol@users.noreply.github.com>
2022-01-13 17:38:31 +0900

    タスクの完了機能作成中

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  // テキストボックス
+  // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグ(div)を未完了リストから完了リストに移動
+    const completeTarget = completeButton.parentNode;
+    const createDiv = document.createElement("div");
+    createDiv.className = "";
+
+    // 未完了リストから削除
+    document.getElementById("incomplete-list").removeChild(completeTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
