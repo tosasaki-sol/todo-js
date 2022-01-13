3c72a2fa834ba68d9fede7970b322d5ec61b73d4
tosasaki-sol <97575304+tosasaki-sol@users.noreply.github.com>
2022-01-13 14:10:01 +0900

    タスクの追加機能（テキストの途中

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// 変更のテスト
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックス
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+
+document
+  .getElementById("add-button") // イベントを付与
+  .addEventListener("click", () => onClickAdd()); // クリックされるとこの関数を呼ぶ
